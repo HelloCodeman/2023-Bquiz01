13dfa925510e3c6b02de7ea99dc7d29fa07f1e91
HelloCodeman <955khm@gmail.com>
2023-12-08 14:47:52 +0800

    完成標題圖片編輯功能

---
+++ api/edit.php
@@ -0,0 +1,19 @@
+<?
+include_once "db.php";
+
+$table = $_POST['table'];
+$DB = ${ucfirst($table)};
+unset($_POST['table']);
+
+foreach ($_POST['text'] as $id => $text) {
+    if (isset($_POST['del']) && in_array($id, $_POST['del'])) {
+        $DB->del($id);
+    } else {
+        $row = $DB->find($id);
+        $row['text'] = $text;
+        $row['display'] = (isset($_POST['display']) && $_POST['display'] == $id) ? 1 : 0;
+        $DB->save($row);
+    }
+}
+
+to("../admin.php?do-$table");
