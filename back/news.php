ee585381151283108982db9e091923f889d22b35
HelloCodeman <955khm@gmail.com>
2023-12-11 11:32:57 +0800

    完成最新消息後台功能

---
+++ back/news.php
@@ -0,0 +1,43 @@
+<div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
+	<p class="t cent botli">最新消息資料管理</p>
+	<form method="post" action="./api/edit.php">
+		<table width="100%" style="text-align: center">
+			<tbody>
+				<tr class="yel">
+					<td width="80%">最新消息資料內容</td>
+					<td width="10%">顯示</td>
+					<td width="10%">刪除</td>
+				</tr>
+				<?php
+
+				$rows = $DB->all();
+				foreach ($rows as $row) {
+				?>
+					<tr>
+						<td>
+							<textarea type="text" name="text[<?= $row['id']; ?>]" style="width:90%;height:60px"><?= $row['text']; ?></textarea>
+						</td>
+						<td>
+							<input type="checkbox" name="display[]" value="<?= $row['id']; ?>" <?= ($row['display'] == 1) ? 'checked' : ''; ?>>
+						</td>
+						<td>
+							<input type="checkbox" name="del[]" value="<?= $row['id']; ?>">
+						</td>
+					</tr>
+				<?php
+				}
+				?>
+			</tbody>
+		</table>
+		<table style="margin-top:40px; width:70%;">
+			<tbody>
+				<tr>
+					<input type="hidden" name="table" value="<?= $do; ?>">
+					<td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?= $do; ?>.php?table=<?= $do; ?>')" value="新增最新消息資料"></td>
+					<td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
+				</tr>
+			</tbody>
+		</table>
+
+	</form>
+</div>
