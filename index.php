e601318e97f9f7feb068c784cf3a390f4c6008eb
HelloCodeman <955khm@gmail.com>
2023-12-15 16:39:32 +0800

    完成更多消息顯示區(not yet)

--- index.php
+++ index.php
@@ -62,54 +62,37 @@
 			// 無法解決亂打網址(亂打會出現錯誤亂碼)
 			*/
 			?>
-			<div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
-			<script>
-				$(".sswww").hover(
-					function() {
-						$("#alt").html("" + $(this).children(".all").html() + "").css({
-							"top": $(this).offset().top - 50
-						})
-						$("#alt").show()
-					}
-				)
-				$(".sswww").mouseout(
-					function() {
-						$("#alt").hide()
-					}
-				)
-			</script>
-			<div class="di di ad" style="height:540px; width:23%; padding:0px; margin-left:22px; float:left; ">
-				<!--右邊-->
-				<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo(&#39;?do=admin&#39;)">管理登入</button>
-				<div style="width:89%; height:480px;" class="dbor">
-					<span class="t botli">校園映象區</span>
-					<script>
-						var nowpage = 0,
-							num = 0;
+			<!--右邊-->
+			<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo(&#39;?do=admin&#39;)">管理登入</button>
+			<div style="width:89%; height:480px;" class="dbor">
+				<span class="t botli">校園映象區</span>
+				<script>
+					var nowpage = 0,
+						num = 0;
 
-						function pp(x) {
-							var s, t;
-							if (x == 1 && nowpage - 1 >= 0) {
-								nowpage--;
-							}
-							if (x == 2 && (nowpage + 1) * 3 <= num * 1 + 3) {
-								nowpage++;
-							}
-							$(".im").hide()
-							for (s = 0; s <= 2; s++) {
-								t = s * 1 + nowpage * 1;
-								$("#ssaa" + t).show()
-							}
+					function pp(x) {
+						var s, t;
+						if (x == 1 && nowpage - 1 >= 0) {
+							nowpage--;
 						}
-						pp(1)
-					</script>
-				</div>
+						if (x == 2 && (nowpage + 1) * 3 <= num * 1 + 3) {
+							nowpage++;
+						}
+						$(".im").hide()
+						for (s = 0; s <= 2; s++) {
+							t = s * 1 + nowpage * 1;
+							$("#ssaa" + t).show()
+						}
+					}
+					pp(1)
+				</script>
 			</div>
 		</div>
-		<div style="clear:both;"></div>
-		<div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
-			<span class="t" style="line-height:123px;"><?= $Bottom->find(1)['bottom']; ?></span>
-		</div>
+	</div>
+	<div style="clear:both;"></div>
+	<div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
+		<span class="t" style="line-height:123px;"><?= $Bottom->find(1)['bottom']; ?></span>
+	</div>
 	</div>
 </body>
 
