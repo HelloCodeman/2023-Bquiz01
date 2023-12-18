81289b0304bc51f6638af1be334b6f4d1306b26d
HelloCodeman <955khm@gmail.com>
2023-12-18 09:13:29 +0800

    將校園映像圖片取出在頁面上呈現

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
-<?php include_once "./api/db.php" ?>
+<?php include_once './api/db.php'; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0040)http://127.0.0.1/test/exercise/collage/? -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
 	<title>卓越科技大學校園資訊系統</title>
 	<link href="./css/css.css" rel="stylesheet" type="text/css">
 	<script src="./js/jquery-1.9.1.min.js"></script>
@@ -23,9 +24,7 @@
 		$title = $Title->find(['display' => 1]);
 		?>
 		<a title="<?= $title['text']; ?>" href="index.php">
-			<div class="ti" style="background:url(&#39;./img/<?= $title['img']; ?>&#39;); background-size:cover;">
-			</div>
-			<!--標題-->
+			<div class="ti" style="background:url(&#39;./img/<?= $title['img']; ?>&#39;); background-size:cover;"></div><!--標題-->
 		</a>
 		<div id="ms">
 			<div id="lf" style="float:left;">
@@ -34,13 +33,12 @@
 					<span class="t botli">主選單區</span>
 				</div>
 				<div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
-					<span class="t">進站總人數 :<?= $Total->find(1)['total']; ?></span>
+					<span class="t">進站總人數 : <?= $Total->find(1)['total']; ?></span>
 				</div>
 			</div>
+
 			<?php
-			// include "./front/main.php";
-			?>
-			<?php
+
 			$do = $_GET['do'] ?? 'main';
 			$file = "./front/{$do}.php";
 			if (file_exists($file)) {
@@ -48,52 +46,56 @@
 			} else {
 				include "./front/main.php";
 			}
-			/*
-			switch ($do) {
-				case "login":
-					include "./front/login.php";
-					break;
-				case "news":
-					include "./front/news.php";
-					break;
-				default:
-					include "./front/main.php";
-			}
-			// 無法解決亂打網址(亂打會出現錯誤亂碼)
-			*/
-			?>
-			<!--右邊-->
-			<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo(&#39;?do=admin&#39;)">管理登入</button>
-			<div style="width:89%; height:480px;" class="dbor">
-				<span class="t botli">校園映象區</span>
-				<script>
-					var nowpage = 0,
-						num = 0;
 
-					function pp(x) {
-						var s, t;
-						if (x == 1 && nowpage - 1 >= 0) {
-							nowpage--;
-						}
-						if (x == 2 && (nowpage + 1) * 3 <= num * 1 + 3) {
-							nowpage++;
-						}
-						$(".im").hide()
-						for (s = 0; s <= 2; s++) {
-							t = s * 1 + nowpage * 1;
-							$("#ssaa" + t).show()
-						}
+			?>
+
+			<div class="di di ad" style="height:540px; width:23%; padding:0px; margin-left:22px; float:left; ">
+				<!--右邊-->
+				<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo(&#39;?do=login&#39;)">管理登入</button>
+				<div style="width:89%; height:480px;" class="dbor">
+					<span class="t botli">校園映象區</span>
+					<?php
+					$imgs = $Image->all(['display' => 1]);
+
+					foreach ($imgs as $idx => $img) {
+					?>
+						<div id="ssaa<?= $idx; ?>" class='im'>
+							<img src="./img/<?= $img['img']; ?>" style="width:150px;height:103px">
+						</div>
+					<?php
 					}
-					pp(1)
-				</script>
+					?>
+					<script>
+						var nowpage = 1,
+							num = 0;
+
+						function pp(x) {
+							var s, t;
+							if (x == 1 && nowpage - 1 >= 0) {
+								nowpage--;
+							}
+							if (x == 2 && (nowpage + 1) * 3 <= num * 1 + 3) {
+								nowpage++;
+							}
+
+							$(".im").hide()
+							for (s = 0; s <= 2; s++) {
+								t = s * 1 + nowpage * 1;
+								$("#ssaa" + t).show()
+							}
+						}
+
+						pp(2)
+					</script>
+				</div>
 			</div>
 		</div>
+		<div style="clear:both;"></div>
+		<div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
+			<span class="t" style="line-height:123px;"><?= $Bottom->find(1)['bottom']; ?></span>
+		</div>
 	</div>
-	<div style="clear:both;"></div>
-	<div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
-		<span class="t" style="line-height:123px;"><?= $Bottom->find(1)['bottom']; ?></span>
-	</div>
-	</div>
+
 </body>
 
 </html>
