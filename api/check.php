c6f95dd57b7175e846300ac26ce3cadb6d18e435
HelloCodeman <955khm@gmail.com>
2024-02-26 09:05:40 +0800

    登入程式增加對表單內容的安全處理

--- api/check.php
+++ api/check.php
@@ -1,10 +1,14 @@
 <?php
 include_once "db.php";
 
+//過濾表單資料
+$acc = htmlspecialchars($_POST['acc']);
+$pw = htmlspecialchars($_POST['pw']);
+
 // 用count不用find，因為後者會回傳資料，降低效能，且帳密是機密資料，怕在回傳時有外洩問題
 
-if ($Admin->count(['acc' => $_POST['acc'], 'pw' => $_POST['pw']]) > 0) {
-    $_SESSION['login'] = $_POST['acc'];
+if ($Admin->count(['acc' => $acc, 'pw' => $pw]) > 0) {
+    $_SESSION['login'] = $acc;
     to("../admin.php");
 } else {
     to("../index.php?do=login&error=帳號或密碼錯誤");
